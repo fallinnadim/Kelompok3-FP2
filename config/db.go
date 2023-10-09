7254bdb2c7f904150c5479a478af9133daff0513
fallinnadim <nadim@mhs.mdp.ac.id>
2023-10-09 07:47:09 +0700

    goose migration

--- config/db.go
+++ config/db.go
@@ -25,29 +25,5 @@ func StartDB() *sql.DB {
 		panic(err)
 	}
 	log.Println("Successfully connected to database")
-	query := `
-	CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		username VARCHAR(50) NOT NULL UNIQUE,
-		email VARCHAR(50) NOT NULL UNIQUE,
-		password VARCHAR(255) NOT NULL,
-		age SMALLSERIAL NOT NULL,
-		created_at DATE NOT NULL,
-		updated_at DATE NOT NULL
-	);
-	CREATE TABLE IF NOT EXISTS social_medias (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(50) NOT NULL,
-		social_media_url VARCHAR(50) NOT NULL,
-		user_id SERIAL NOT NULL,
-		CONSTRAINT fk_user_id
-  		FOREIGN KEY(user_id)
-  		REFERENCES users(id)
-	);
-	`
-	_, errQuery := db.Exec(string(query))
-	if errQuery != nil {
-		log.Panicln("Failed to Create table")
-	}
 	return db
 }
---
+++ database/migrations/20231009072330_create_social_medias_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS social_medias (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50) NOT NULL,
+		social_media_url VARCHAR(50) NOT NULL,
+		user_id SERIAL NOT NULL,
+		CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE social_medias;
+-- +goose StatementEnd
