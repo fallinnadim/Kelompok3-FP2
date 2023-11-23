f993cdfe3805849d50150f1c5930382eee3a0e68
fallinnadim <nadim@mhs.mdp.ac.id>
2023-11-23 22:59:47 +0700

    hehehe

--- infra/postgres/db.go
+++ infra/postgres/db.go
@@ -3,12 +3,18 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func GetDbInstance() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env filezzz")
+	}
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"))
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
