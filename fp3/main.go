3e9023e4210588b4f18abf9a853b94e66702e9e3
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-25 19:40:20 +0700

    Refactoring

--- fp3/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/adenhidayatuloh/Kelompok5_FinalProject3/handler"
-
-func main() {
-	handler.StartApp()
-
-}
--- fp4/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/MSyabdewa/Kelompok5_FinalProject4/handler"
-
-func main() {
-	handler.StartApp()
-}
